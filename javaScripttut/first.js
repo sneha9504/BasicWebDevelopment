9b79f696bf07d0e814f1b583a9293dc7eb37e356
Sneha Wani <snehawani4321@gmail.com>
2025-07-22 13:43:35 +0530

    js practice

---
+++ javaScripttut/first.js
@@ -0,0 +1,99 @@
+// const greeting = function (){
+//     console.log("hello everyone");
+// }
+
+// greeting();
+
+// function addition(a,b){
+//     return a+b;
+
+// }
+// let sum = addition(10,2);
+// console.log(sum);
+
+// let greets =()=>{console.log("good mornng")}
+
+// greets();
+// const robot = {
+//     name: "Bender",
+
+//     announceRegular: function () {
+//         console.log("Hi, I am " + this.name);
+//         setTimeout(function () { 
+//             console.log("Regular: Still " + this.name);
+//         }, 100);
+//     },
+
+//     announceArrow: function () {
+//         console.log("Hello, I am " + this.name);
+//         setTimeout(() => {
+//             console.log("Arrow: Still " + this.name);
+//         }, 100);
+//     }
+// };
+// robot.announceRegular();
+// robot.announceArrow();
+
+// let arr = [];
+
+// arr.push(10);
+// arr.push(20);
+
+// console.log(arr.length);
+// console.log(arr);
+
+// arr.splice(1,1,99);
+// console.log(arr);
+
+
+const students = ["Alice", "Bob", "Charlie"];
+// for (let i = 0; i < students.length; i++) {
+// console.log(`Student at index ${i}: ${students[i]}`);
+// }
+// for (const student of students) {
+// console.log(student);
+// }
+
+// students.forEach(function(student, index) {
+// console.log(`${student} is at position ${index}`);
+// });
+
+// students.forEach((student) => console.log(student));
+
+
+// const headChefCookbook = {
+//     makeFancyOmelette: function () {
+//         console.log("Making a super fancy omelette! 🍳");
+//     },
+//     greetCustomer: function () {
+//         console.log("Welcome to our restaurant!");
+//     }
+// };
+
+// const traineeJoe = {
+//     name: "Joe",
+//     makeSandwich: function () {
+//         console.log(this.name + " is making a delicious sandwich! 🥪");
+//     }
+// };
+// traineeJoe.makeSandwich();
+
+// const janeTheTrainee = Object.create(headChefCookbook);
+// janeTheTrainee.name = "Jane"; // Jane has her own name
+// janeTheTrainee.makeFancyOmelette(); // Output: Making a super fancy omelette!
+
+// janeTheTrainee.greetCustomer(); // Output: Welcome to our restaurant!
+// // Also from the cookbook!
+// // What if Jane also knows how to make something specific?
+// janeTheTrainee.makeAwesomeSmoothie = function () {
+//     console.log(this.name + " is blending an awesome smoothie! 🥤");
+// };
+// janeTheTrainee.makeAwesomeSmoothie();
+
+
+console.log("First: I'm making a sandwich."); // 1st
+setTimeout(() => {
+    console.log("Third: Sandwich is ready! 🥪"); // 3rd
+   
+}, 2000); 
+console.log("Second: I'm washing the dishes while I wait."); // 2nd
